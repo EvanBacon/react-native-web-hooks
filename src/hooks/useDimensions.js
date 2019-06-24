18f94def8bb2fa33679b150d772833558da599f3
Evan Bacon <baconbrix@gmail.com>
2019-06-24 13:56:33 -0700

    fixed dimensions

--- src/hooks/useDimensions.js
+++ src/hooks/useDimensions.js
@@ -1,3 +1,21 @@
-export function useREM(multiple) {
-  return parseInt(getComputedStyle(document.documentElement).fontSize) * multiple;
-}
+import * as React from 'react';
+import { Dimensions } from 'react-native';
+
+export default () => {
+  const [dimensions, setDimensions] = React.useState({
+    window: Dimensions.get('window'),
+    screen: Dimensions.get('screen'),
+  });
+
+  const onChange = ({ window, screen }) => {
+    setDimensions({ window, screen });
+  };
+
+  React.useEffect(() => {
+    Dimensions.addEventListener('change', onChange);
+
+    return () => Dimensions.removeEventListener('change', onChange);
+  }, []);
+
+  return dimensions;
+};
