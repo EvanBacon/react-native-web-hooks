36316d47490374db66530e24a71e85ce4a262990
Evan Bacon <baconbrix@gmail.com>
2020-01-07 14:53:24 -0800

    2.0.2 fix native useREM

--- src/hooks/useREM.ts
+++ src/hooks/useREM.ts
@@ -1,5 +1,5 @@
 import { PixelRatio } from 'react-native';
 
 export default function useREM(multiple: number): string | number {
-  return PixelRatio.getFontScale() * multiple;
+  return PixelRatio.getFontScale() * 16 * multiple;
 }
